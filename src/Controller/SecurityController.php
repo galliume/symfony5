1d31b5ea431646b431c3314f5f3174cc4a3dc0f8
Guillaume Percepied <gcconantc@gmail.com>
2020-02-03 17:33:50 +0100

    WIP Token API security

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): JsonResponse
+    {
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        return $this->json([
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
