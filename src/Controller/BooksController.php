1fc6a856e74b34e969bf58005d97a81752b9090d
Guillaume Percepied <gcconantc@gmail.com>
2020-02-03 13:58:51 +0100

    Get book from db

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Book;
 
 class BooksController extends AbstractController
 {
     /**
-     * @Route("/books", name="books")
+     * @Route("/index", name="index")
      */
     public function index()
     {
@@ -18,5 +21,15 @@ class BooksController extends AbstractController
                 ['ISBN' => '1-905328-14-1', 'title' => 'Dracula', 'author' => 'Bram Stoker'],
             ]
         ]);
+    }
+
+    /**
+     * @Route("/books", name="list_books")
+     */
+    public function listBook(): JsonResponse
+    {
+        $books =  $this->getDoctrine()->getRepository(Book::class)->findAll();
+
+        return $this->json(['books' => $books]);
     }
 }
---
+++ src/Entity/Book.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BookRepository")
+ */
+class Book implements \JsonSerializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=13)
+     */
+    private $isbn;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            "id" => $this->getId(),
+            "isbn" => $this->getIsbn(),
+            "title" => $this->getTitle(),
+            "author" => $this->getAuthor()
+        ];
+    }
+}
